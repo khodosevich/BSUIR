06cca47d89f66274f749eec56342c36ffa053798
Matvey <hodos.952795@gmail.com>
2023-09-08 22:24:10 +0300

    add validation for numberStr

--- 5 term/Scala/lab1/generateNumbers.scala
+++ 5 term/Scala/lab1/generateNumbers.scala
@@ -59,48 +59,59 @@ object Variant2  {
     result.toString()
   }
 
+  def inputNumber() : String = {
+
+      var numberStr : String = "";
+
+      print ("Enter the number: ")
+      numberStr = scala.io.StdIn.readLine ()
+
+      if ((numberStr.length () < 10) ) {
+      println ("Incorrect number");
+      sys.exit (0)
+    } else {
+      numberStr.substring (0, 10)
+    }
+
+      val isNumeric = numberStr.matches ("^[0-9]+$")
+
+      if (! isNumeric) {
+      println ("Строка содержит символы.")
+      sys.exit (0)
+     }
+      numberStr
+  }
+
   def main(args: Array[String]): Unit = {
 
-    print("Enter the number: ")
-
-    var numberString = ""
     var a: Long = 0
     var b : Long = 0
     var c : Long = 0
-    var rez: Long = 0
+    var rez: Float = 0
     var z : Long = 0
 
-    numberString = scala.io.StdIn.readLine()
-
-    if ((numberString.length() < 10)) {
-      println("Incorrect number");
-      sys.exit(0)
-    }else{
-      numberString.substring(0,10)
-    }
-
-    val isNumeric = numberString.matches("^[0-9]+$")
-
-    if (!isNumeric) {
-      println("Строка содержит символы, отличные от цифр.")
-      sys.exit(0)
-    }
+    var numberString: String = inputNumber();
 
 
     for (i <- 1 to 5) {
-      a = substringFromSymbolsAtEvenPositions(numberString).toInt
+      a = substringFromSymbolsAtEvenPositions(numberString).toLong
 
-      b = substringFromSymbolsAtOddPositions(numberString).toInt
+      b = substringFromSymbolsAtOddPositions(numberString).toLong
       c = a * b
 
-      rez = c.toString().substring(0, 3).toLong / 1000
+      rez = c.toString().substring(0, 3).toFloat / 1000
 
       z += c
 
-      println(s"\na = ${a}  b = ${b}   c =  ${c}  rez =  ${rez} z = ${z}" );
+      if(z.toString.length > 11) z = z.toString.substring(0,10).toLong;
 
-      numberString = numberString.substring(1, numberString.length() - 1)
+
+      println(s"\na = ${a}  b = ${b}   c = ${c}  rez = ${rez} z = ${z}" );
+
       numberString = "011" + z
+      numberString = numberString.substring(0,10)
+
+      println(numberString)
 
     }
   }
