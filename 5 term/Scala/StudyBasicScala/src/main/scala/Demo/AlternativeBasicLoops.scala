5d9cdf0607536df85dcbf5dc16eea92f6755235d
Matvey <hodos.952795@gmail.com>
2023-09-11 16:46:51 +0300

    refactoring code

--- 5 term/Scala/StudyBasicScala/src/main/scala/Demo/AlternativeBasicLoops.scala
+++ 5 term/Scala/StudyBasicScala/src/main/scala/Demo/AlternativeBasicLoops.scala
@@ -1,5 +1,42 @@
 package Demo
 
+import scala.annotation.tailrec
+
 object AlternativeBasicLoops {
+
+  def fact( n :Int ):BigInt ={
+    assert(n >= 0, "ожидается  number > 0")
+    assert(n < 50000, "слишком большое число")
+
+    @tailrec
+    def f(n:Int, acc : BigInt = 1):BigInt =
+      if(n > 0) f(n - 1 , acc * n)
+      else acc
+
+    f(n)
+  }
+
+  def fact1( n : Int) = {
+    assert(n >= 0, "ожидается  number > 0")
+    assert(n < 50000, "слишком большое число")
+
+    if (n == 0) 1
+    else (1 to n).foldLeft(BigInt(1)) ( _ * _ )
+  }
+
+  def fact2(n: Int) = {
+    assert(n >= 0, "ожидается  number > 0")
+    assert(n < 50000, "слишком большое число")
+
+    if (n == 0) 1
+//    else (BigInt(1) to BigInt(n)).reduce(_ * _)
+    else (BigInt(1) to BigInt(n)).product
+  }
+
+  def main(args: Array[String]): Unit = {
+     println(fact(5))
+     println(fact1(5))
+     println(fact2(5))
+  }
 
 }
--- 5 term/ИИУВМ/lab1.cpp
+++ 5 term/ИИУВМ/lab1.cpp
@@ -1,12 +1,13 @@
 ﻿#include <iostream>
-#include <Windows.h>
+#include <windows.h>
 #include <powrprof.h>
 #include <winbase.h>
+#pragma comment(lib, "Powrprof.lib")
 
 using namespace std;
 
 void printInfo() {
-    cout << "1 - charging status" << endl ;
+    cout << "1 - charging status" << endl;
     cout << "2 - battary status" << endl;
     cout << "3 - battary procent % " << endl;
     cout << "4 - status of battary" << endl;
@@ -14,49 +15,50 @@ void printInfo() {
     cout << "6 - hibernation" << endl;
     cout << "7 - the number of seconds of battery life remaining" << endl;
     cout << "8 - the number of seconds of battery life when at full charge" << endl;
+    cout << "9 - update state" << endl;
     cout << "0 - the end" << endl;
 }
 
-void functionForACLineStatus(int state){
-    
+void chargingStatus(int state) {
+
     switch (state) {
-    case 0:
-        cout << "dont connect to charge" << endl;
-        break;
-    case 1:
-        cout << "connect to charge" << endl;
-        break;
-    case 255:
-        cout << "Unknown status" << endl;
-        break;
-    default:
-        break;
+        case 0:
+            cout << "dont connect to charge" << endl;
+            break;
+        case 1:
+            cout << "connect to charge" << endl;
+            break;
+        case 255:
+            cout << "Unknown status" << endl;
+            break;
+        default:
+            break;
     }
 }
-void functionForBatteryFlag(int battaryStatus) {
+void battaryState(int battaryStatus) {
 
     switch (battaryStatus) {
-    case 1:
-        cout << "High  the battery capacity is at more than 66 percent" << endl;
-        break;
-    case 2:
-        cout << "Low  the battery capacity is at less than 33 percent" << endl;
-        break;
-    case 4:
-        cout << "Critical—the battery capacity is at less than five percent" << endl;
-        break;
-    case 8:
-        cout << "Charging" << endl;
-        break;
-    case 128:
-        cout << "No system battery" << endl;
-        break;
-    case 255:
-        cout << "Unknown status—unable to read the battery flag information" << endl;
-        break;
-    default:
-        cout << "nothing" << endl;
-        break;
+        case 1:
+            cout << "High the battery capacity is at more than 66 percent" << endl;
+            break;
+        case 2:
+            cout << "Low the battery capacity is at less than 33 percent" << endl;
+            break;
+        case 4:
+            cout << "Critical the battery capacity is at less than five percent" << endl;
+            break;
+        case 8:
+            cout << "Charging" << endl;
+            break;
+        case 128:
+            cout << "No system battery" << endl;
+            break;
+        case 255:
+            cout << "Unknown status—unable to read the battery flag information" << endl;
+            break;
+        default:
+            cout << "nothing" << endl;
+            break;
     }
 }
 void battaryProcent(int procent) {
@@ -70,75 +72,93 @@ void battaryProcent(int procent) {
 void statusOfBattary(int statusBattary) {
 
     switch (statusBattary) {
-    case 0:
-        cout << "Battery saver is off." << endl;
-        break;
-    case 1:
-        cout << "Battery saver on. Save energy where possible." << endl;
-        break;
-    default:
-        break;
+        case 0:
+            cout << "Battery saver is off." << endl;
+            break;
+        case 1:
+            cout << "Battery saver on. Save energy where possible." << endl;
+            break;
+        default:
+            break;
     }
+}
+
+
+SYSTEM_POWER_STATUS getState(SYSTEM_POWER_STATUS &computer) {
+
+
+    if (!GetSystemPowerStatus(&computer)) {
+        cout << "truble with getting data from powrprof" << endl;
+        exit(1);
+    }
+
+    return computer;
+
 }
 
 int main()
 {
     SYSTEM_POWER_STATUS computer{};
 
-    GetSystemPowerStatus(&computer);
-    
+
+    computer = getState(computer);
+
+
     cout << endl;
     cout << "\t";
     battaryProcent(computer.BatteryLifePercent);
     cout << "\t";
-    functionForACLineStatus(computer.ACLineStatus);
+    chargingStatus(computer.ACLineStatus);
     cout << "\t";
-    functionForBatteryFlag(computer.BatteryFlag);
+    battaryState(computer.BatteryFlag);
     cout << "\t";
     statusOfBattary(computer.SystemStatusFlag);
     cout << endl;
-    
+
     int work = 1000;
     while (1)
     {
         printInfo();
-        
+
         cout << "\nchoose a operation: ";
         cin >> work;
 
         switch (work)
         {
-        case 1:
-            functionForACLineStatus(computer.ACLineStatus);
-            break;
-        case 2:
-            functionForBatteryFlag(computer.BatteryFlag);
-            break;
-        case 3:
-            battaryProcent(computer.BatteryLifePercent);
-            break;
-            break;
-        case 4:
-            statusOfBattary(computer.SystemStatusFlag);
-            break;
-        case 5:
-            SetSuspendState(FALSE, FALSE, FALSE);
-            break;
-        case 6:
-            SetSuspendState(TRUE, FALSE, FALSE);
-            break;
-        case 7: {
-            int timeSecond = computer.BatteryLifeTime;
-            cout << "hours until the end of work: " << timeSecond / 60 / 60 << endl;
-            break;
-        }
-        case 8: {
-            int time = computer.BatteryFullLifeTime;
-            cout << "the number of seconds of battery life when at full charge: " << time << endl;
-            break;
-        }
-        default:
-            break;
+            case 1:
+                chargingStatus(computer.ACLineStatus);
+                break;
+            case 2:
+                battaryState(computer.BatteryFlag);
+                break;
+            case 3:
+                battaryProcent(computer.BatteryLifePercent);
+                break;
+                break;
+            case 4:
+                statusOfBattary(computer.SystemStatusFlag);
+                break;
+            case 5: {
+                SetSuspendState(FALSE, FALSE, FALSE);
+                break;
+            }
+            case 6:
+                SetSuspendState(TRUE, FALSE, FALSE);
+                break;
+            case 7: {
+                cout << "hours until the end of work: " << (double)computer.BatteryLifeTime / 60 / 60 << endl;
+                break;
+            }
+            case 8: {
+                cout << "the number of days of battery life when at full charge: " <<(double)computer.BatteryFullLifeTime / 60 / 60 /24 << endl;
+                break;
+            }
+            case 9: {
+                cout << "update" << endl;
+                computer = getState(computer);
+            }
+            default:
+                break;
         }
 
         if (work == 0) {
