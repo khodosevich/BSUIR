4b0303e057a01464d9a92838ab296e695772fdc6
Matvey <hodos.952795@gmail.com>
2023-10-31 21:41:58 +0300

    структурная схема

--- 5 term/Scala/lab4/lab4/src/main/scala/task1/Doptask1.scala
+++ 5 term/Scala/lab4/lab4/src/main/scala/task1/Doptask1.scala
@@ -1,20 +1,50 @@
+
 package task1
 
-import org.apache.spark.sql.{Encoders, SparkSession}
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 
-object Doptask1 extends App{
+// два предложения на англ. удалить пробелы 2 способами через регулярки
+// утроить букву T в слове mother
+// удалить все гласные с словах
 
- var encoder = Encoders.kryo[String]
-  val spark = SparkSession.builder().master("local[*]").appName("lab4").getOrCreate()
-
-  val data = spark.read.textFile("src/main/scala/task1/text")
-
-  val wordsRDD = data.flatMap(l => l.split("\\s"))(encoder)
-
-  val filterData = wordsRDD.filter(word => word.endsWith("ing"))
-
-  filterData.foreach(el => println(el))
+object SparpDop {
 
 
+ def deleteSpacesFirst():String={
+  val sentenceWithoutSpaces1 = RDD_sentence.map(line => line.replaceAll("\\s", ""))
+    println("\nSentence without spaces:")
+    sentenceWithoutSpaces1.foreach(println)
+ }
 
+ def deleteSpacesSecond(): String = {
+  val sentenceWithoutSpaces2 = RDD_sentence.map(line => line.replaceAll(" ", ""))
+    println("\nSentence without spaces:")
+    sentenceWithoutSpaces2.foreach(println)
+ }
+
+ def main(args : Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("SparkLab")
+    val sc = new SparkContext(conf)
+
+  val sentence = "This is a sample sentence with spaces. My name is Max."
+  val RDD_sentence = sc.parallelize(Seq(sentence))
+
+    deleteSpacesFirst()
+    deleteSpacesSecond()
+
+    // утроение буквы Т
+    val word = "Mother"
+    val wordWithTripleT = sc.parallelize(Seq(word)).map(line => line.replaceAll("t", "ttt"))
+    println("\nWord with triple 't':")
+    wordWithTripleT.foreach(println)
+
+    // удаление гласных
+    val sentenceWithoutVowels = RDD_sentence.map(line => line.replaceAll("[aeiouyAEIOUY]", ""))
+    println("\nSentence without vowels:")
+    sentenceWithoutVowels.foreach(println)
+ }
 }
+
